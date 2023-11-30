49dc5d85b2a5345dcabe50704e6c02d20784ab4d
Ahmed Bakr <114930002+AhmedBakrXI@users.noreply.github.com>
2023-11-30 19:56:39 +0200

    Add files via upload

---
+++ src/main/java/com/editor/xml_editor/MainApplication.java
@@ -0,0 +1,24 @@
+package com.editor.xml_editor;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("app.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+        stage.setTitle("XML Editor");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
