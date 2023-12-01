3efe4c99a3b365f81397808f1feb9f0d15458ace
adhamkhaled312 <105177075+adhamkhaled312@users.noreply.github.com>
2023-12-01 13:19:15 +0200

    Update Post class

--- src/main/java/com/editor/xml_editor/Post.java
+++ src/main/java/com/editor/xml_editor/Post.java
@@ -1,4 +1,36 @@
 package com.editor.xml_editor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Post {
+    private String body;
+    private List<String> topics;
+
+    Post() {
+        topics = new ArrayList<>();
+    }
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public List<String> getTopics() {
+        return topics;
+    }
+
+    public void setTopics(List<String> topics) {
+        this.topics = topics;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "body='" + body + '\'' +
+                ", topics=" + topics +
+                '}';
+    }
 }
