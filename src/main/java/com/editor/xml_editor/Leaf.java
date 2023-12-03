e3a37fadaab37798ebba7c84596b44b042841aae
Ahmed Khaled <130765437+Ahmed-Khaled-Abdelmaksod@users.noreply.github.com>
2023-12-03 23:22:02 +0200

    Update Leaf.java

--- src/main/java/com/editor/xml_editor/Leaf.java
+++ src/main/java/com/editor/xml_editor/Leaf.java
@@ -1,3 +1,9 @@
+/*
+ * @author : Ahmed Khaled Abdelmaksod Ebrahim
+ * @date   : 3 DEC 2023
+ * @brief  : contains the class Leaf
+*/
+
 public class Leaf extends Node{
     private final char character;
 
