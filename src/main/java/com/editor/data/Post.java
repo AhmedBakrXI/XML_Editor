e37e4d0c4849d48efe4860a9af0ee640871b93b5
Ahmed Bakr <114930002+AhmedBakrXI@users.noreply.github.com>
2023-12-10 13:03:10 +0200

    Add files via upload

---
+++ src/main/java/com/editor/data/Post.java
@@ -0,0 +1,37 @@
+package com.editor.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Post {
+    private String body;
+    private List<String> topics;
+
+    Post() {
+        topics = new ArrayList<>();
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public List<String> getTopics() {
+        return topics;
+    }
+
+    public void setTopics(List<String> topics) {
+        this.topics = topics;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "body='" + body + '\'' + "\n" +
+                ", topics=" + topics + "\n" +
+                '}';
+    }
+}
