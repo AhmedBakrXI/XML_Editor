719e321c2588324a7205a90e8482ddc8ed215ca0
Ahmed Bakr <114930002+AhmedBakrXI@users.noreply.github.com>
2023-12-28 11:45:43 +0200

    add project launcher

---
+++ src/main/java/com/editor/Launcher.java
@@ -0,0 +1,9 @@
+package com.editor;
+
+import com.editor.xml_editor.MainApplication;
+
+public class Launcher {
+    public static void main(String[] args) {
+        MainApplication.main(args);
+    }
+}
